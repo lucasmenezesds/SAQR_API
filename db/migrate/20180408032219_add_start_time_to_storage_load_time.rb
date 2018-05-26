69e31ef193ef0d13bb5e331027dabdf17b2d9591
Lucas Menezes <lucasmpx2000@gmail.com>
2018-05-26 04:05:18 -0300

    [FIX] Changed the schema to use datetime instead of time on start_time
    Fixed some tables/columns (in the wrong way)
    - Changed the t.time to t.datetime, did some changes in the migrations just changing the old one instead of creating new migrations
    *Tables affected*
    - storage_time
    - load_time
    - receive_time
    - picking_time
    - transportation_time

--- db/migrate/20180408032219_add_start_time_to_storage_load_time.rb
+++ db/migrate/20180408032219_add_start_time_to_storage_load_time.rb
@@ -1,7 +1,8 @@
 class AddStartTimeToStorageLoadTime < ActiveRecord::Migration[5.1]
   def change
-    add_column :storage_times, :start_time, :time
-    add_column :load_times, :start_time, :time
-    add_column :receive_times, :start_time, :time
+    add_column :storage_times, :start_time, :datetime
+    add_column :load_times, :start_time, :datetime
+    add_column :receive_times, :start_time, :datetime
+    add_column :transportation_times, :start_time, :datetime
   end
 end
