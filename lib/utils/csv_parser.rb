ae1a67f74ca9f787caea484c4544b9a7d0597bff
Lucas Menezes <lucasmpx2000@gmail.com>
2018-06-10 07:10:18 -0300

    [NEW] Created CsvParser Util, Added list of brazilians cities to import
    - Created the CSV Parser and it tests to help to populate the DB
    - Added 2 files
    -- List of Brazilians Cities to populate the database
    -- csv_mock.csv as fixture to help the test

---
+++ lib/utils/csv_parser.rb
@@ -0,0 +1,29 @@
+require 'csv'
+require 'rails'
+
+# Class responsible to parse CSVs
+class CsvParser
+  attr_reader :files_path
+
+  def initialize(path)
+    @files_path = path
+  end
+
+  def parse_file(file_name)
+    path = "#{@files_path}/#{file_name}"
+    CSV.table(path)
+  end
+
+  def to_hash(csv_row)
+    csv_row.to_h.stringify_keys
+  end
+
+  def to_array_of_hashes(csv_table)
+    final_array = []
+    csv_table.each do |csv_row|
+      final_array << to_hash(csv_row)
+    end
+
+    final_array
+  end
+end
