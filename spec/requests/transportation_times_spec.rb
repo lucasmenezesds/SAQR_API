62f4cdbea7429288d84c9bfb61f9c4bddaf890db
Lucas Menezes <lucasmpx2000@gmail.com>
2018-06-10 23:13:04 -0300

    [UPDATE] Fixed the request tests
    - Improved the request test adding the headers

--- spec/requests/transportation_times_spec.rb
+++ spec/requests/transportation_times_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "TransportationTimes", type: :request do
   describe "GET /transportation_times" do
     it "works! (now write some real specs)" do
-      get transportation_times_path
+      get transportation_times_path, params: {}, headers: { ACCEPT: 'application/vnd.api+json' }
       expect(response).to have_http_status(200)
     end
   end
--- spec/requests/trucks_spec.rb
+++ spec/requests/trucks_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Trucks", type: :request do
   describe "GET /trucks" do
     it "works! (now write some real specs)" do
-      get trucks_path
+      get trucks_path, params: {}, headers: { ACCEPT: 'application/vnd.api+json' }
       expect(response).to have_http_status(200)
     end
   end
