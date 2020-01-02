5d8ae4221c7461deef5407b12e4ea2ce696d4790
Lucas Menezes <lucasmpx2000@gmail.com>
2020-01-02 08:46:04 -0300

    [UPDATE] Improved DistributionMethod serializer

--- app/serializers/distribution_method_serializer.rb
+++ app/serializers/distribution_method_serializer.rb
@@ -2,7 +2,20 @@
 
 # JSON Serializer
 class DistributionMethodSerializer < ActiveModel::Serializer
+
   attribute :id, key: :key
   attribute :short_name, key: :value
   attribute :name, key: :text
+  attribute :distribution_parameters, key: :parameters
+
+  def distribution_parameters
+    parameters =
+      ActiveModelSerializers::SerializableResource.new(
+        object.distribution_parameters,
+        each_serializer: DistributionParameterSerializer,
+        adapter: :json
+      ).serializable_hash
+    parameters[:distribution_parameters]
+  end
+
 end
